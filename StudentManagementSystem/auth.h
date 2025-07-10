9f5c3410a38633f66c90cd38af4ff3bb4fab5322
Ordom <105794218+suky7E@users.noreply.github.com>
2025-07-10 07:46:14 +0700

    Delete StudentManagementSystem directory
    change to branch beta with new update

--- StudentManagementSystem/auth.h
+++
@@ -1,7 +0,0 @@
-#ifndef AUTH_H
-#define AUTH_H
-
-bool login();
-bool signUp();
-
-#endif
--- StudentManagementSystem/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <string>
-using namespace std;
-
-string toLower(const string& str);
-
-#endif
